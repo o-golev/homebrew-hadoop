9ea7204c36dad1c1cb714c65d3149bca197bf3d1
Oleg Golev <o.golev@vk.team>
2021-11-26 17:10:08 +0300

    hadoop and hive new formula

---
+++ Formula/hadoop-2.10.1.rb
@@ -0,0 +1,32 @@
+class Hadoop2101 < Formula
+  desc "Framework for distributed processing of large data sets"
+  homepage "https://hadoop.apache.org/"
+  url "https://archive.apache.org/dist/hadoop/common/hadoop-2.10.1/hadoop-2.10.1.tar.gz"
+  sha256 "273d5fa1d479d0bb96759b16cf4cbd6ba3e7f863a0778cbae55ab83417e961f0"
+  license "Apache-2.0"
+
+  depends_on "openjdk@8"
+
+  conflicts_with "yarn", because: "both install `yarn` binaries"
+  conflicts_with "hadoop", because: "both install `hadoop` binaries"
+
+  def install
+    rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
+    libexec.install %w[bin sbin libexec share etc]
+    Dir["#{libexec}/bin/*"].each do |path|
+      (bin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    end
+    Dir["#{libexec}/sbin/*"].each do |path|
+      (sbin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    end
+    Dir["#{libexec}/libexec/*.sh"].each do |path|
+      (libexec/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    end
+    # Temporary fix until https://github.com/Homebrew/brew/pull/4512 is fixed
+    chmod 0755, Dir["#{libexec}/*.sh"]
+  end
+
+  test do
+    system bin/"hadoop", "fs", "-ls"
+  end
+end
